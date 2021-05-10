67a7939940124e866e4c4cd9fb3d51d04c42524c
musiccity365 <m_sackstein@yahoo.com>
2021-05-10 17:21:43 -0400

    Revert "removal of year dropdown menu"
    This reverts commit e419a169c8599220d387a248535b09635ff66cac.

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 const baseURL = 'http://localhost:3000'
-    // const carsURL = baseURL + '/cars'
+const carsURL = baseURL + '/cars'
 
-// const list = document.getElementById("car-list")
+const list = document.getElementById("car-list")
 
-// const form = document.getElementById("car-form")
+const form = document.getElementById("car-form")
 
-// form.addEventListener('submit', handleFormSubmit)
-document.addEventListener("DOMContentLoaded", () => {
-    fetchCars()
-})
+form.addEventListener('submit', handleFormSubmit)
 
-function fetchCars() {
-    fetch(`${baseURL}/cars`)
-        .then(resp => console.log(resp))
-        .then(cars => {
-            for (const car of cars) {
-                console.log("rails obj", car)
-                let c = new Car(car.id, car.make, car.model, car.color, car.year)
-                console.log("js obj", c)
-            }
-        })
+function handleFormSubmit(e) {
+    e.preventDefault()
+
+    CarApi.createCar()
+    form.reset()
 }
-handleFormSubmit(e) {
-        e.preventDefault()
-
-        // CarApi.createCar()
-        form.reset()
-    }
-    //callback function for cars list
-    // CarApi.getCars()
-
-//callback function for origins list
-// OriginApi.getOrigins()
