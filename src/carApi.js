db01355f777b2dd2e6588807730c77d897fe11e1
musiccity365 <m_sackstein@yahoo.com>
2021-05-10 17:19:00 -0400

    removal of year dropdown menu

--- src/carApi.js
+++ src/carApi.js
@@ -2,16 +2,25 @@ class CarApi {
     // all variables and methods defined must be of type static in API level
     static baseURL = 'http://localhost:3000/cars'
 
-    static getCars() {
-        fetch(this.baseURL)
-            .then(response => response.json())
-            .then(data => {
-                // debugger
-                Car.all.forEach(car => {
-                    const c = new Car(car.make, car.model, car.color, car.year)
-                    c.attachToDom()
-                })
-            })
+    // static getCars() {
+    //     fetch(this.baseURL)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             // debugger
+    //             Car.all.forEach(car => {
+    //                 const c = new Car(car.make, car.model, car.color, car.year)
+    //                 c.attachToDom()
+    //             })
+    //         })
+    // }
+    static createCar() {
+        const formData = {
+            make: makeInput.value,
+            model: modelInput.value,
+            color: colorInput.value,
+            year: yearDropdown.value,
+            origin_id: dropdown.value
+        }
     }
 
 }
